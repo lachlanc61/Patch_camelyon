4250ab681a4ea9be2fc0fa5458d31f181714b891
lachlanc61 <lachlanc.612@gmail.com>
2022-09-05 23:21:51 +1000

    built basic 3-layer model and plot outputs

--- core.py
+++ core.py
@@ -17,15 +17,17 @@ import src.utils as utils
 
 
 """
-Project description here
+Practice project - histology
 
 """
 #-----------------------------------
 #VARIABLES
 #-----------------------------------
+PREPLOT=False     #plot some images before running model
+POSTPLOT=True     #plot accuracy and loss over time
 
-batchlen = 128
-buffer=5000
+batchlen = 256    #size of batch for fitting
+buffer=5000       #buffer for shuffling
 
 #-----------------------------------
 #FUNCTIONS
@@ -45,19 +47,16 @@ def normalise(img, labels):
 #INITIALISE
 #-----------------------------------
 
-starttime = time.time()             #init timer
+starttime = time.time()  #init timer
 print(f'python version: {sys.version}')
 print(f'tensorflow version: {tf.__version__}')
 
 gpu_available = (len(tf.config.list_physical_devices('GPU')) > 0)
 print(f'GPU: {gpu_available}')
+
 #-----------------------------------
 #MAIN START
 #-----------------------------------
-"""
-weird TF bug - datasets going in literal ~ directory rather than home
-set TDFS_DATA_DIR in bashrc - see if this helps next time
-"""
 
 print("---------------")
 data, info = tfds.load('patch_camelyon', with_info = True, as_supervised = True)
@@ -87,25 +86,108 @@ dtest = dtest.batch(batchlen)
 
 timg, tlabels = next(iter(dtrain))
 vimg, vlabels = next(iter(dvalidation))
+testimg, testlabels  = next(iter(dtest))
 
 #check shapes for both
 #should correspond to batch size
 print("labelshape:",vlabels.shape)
 print("imgshape:",timg.shape)
 print("batch size:",batchlen)
+if PREPLOT:
+  #plot 12 random images as RGB, including label as true/false 
+  fig, ax = plt.subplots(2,6, figsize=(12,5))
+  fig.tight_layout(pad=0.1)
 
-#plot 12 random images as RGB, including label as true/false 
-fig, ax = plt.subplots(2,6, figsize=(12,5))
-fig.tight_layout(pad=0.1)
+  for i,ax in enumerate(ax.flat):
+      rand = np.random.randint(batchlen)    
+      ax.imshow(timg[rand,:,:,:])
+      ax.set_title(bool(tlabels.numpy()[rand]))
+      ax.set_axis_off()
 
-for i,ax in enumerate(ax.flat):
-    rand = np.random.randint(batchlen)    
-    ax.imshow(timg[rand,:,:,:])
-    ax.set_title(bool(tlabels.numpy()[rand]))
-    ax.set_axis_off()
+  plt.show()
 
-plt.show()
-#open the data 
+#Initialise basic TF model
+#   flatten RGB images as first layer
+model = Sequential(
+    [
+        keras.layers.Flatten(input_shape=(96, 96, 3)),
+ #      tf.keras.Input(shape=(96,96,3)),    #specify input size
+        ### START CODE HERE ### 
+        keras.layers.Dense(25, activation='sigmoid'),
+        keras.layers.Dense(15, activation='sigmoid'), 
+        keras.layers.Dense(1, activation='sigmoid') 
+        ### END CODE HERE ### 
+    ], name = "my_model" 
+)   
+
+#view model summary
+model.summary()
+
+#print parameters
+L1_num_params = 400 * 25 + 25  # W1 parameters  + b1 parameters
+L2_num_params = 25 * 15 + 15   # W2 parameters  + b2 parameters
+L3_num_params = 15 * 1 + 1     # W3 parameters  + b3 parameters
+print("L1 params = ", L1_num_params, ", L2 params = ", L2_num_params, ",  L3 params = ", L3_num_params )
+
+[layer0, layer1, layer2, layer3] = model.layers
+
+#### Examine Weights & shapes
+W1,b1 = layer1.get_weights()
+W2,b2 = layer2.get_weights()
+W3,b3 = layer3.get_weights()
+print(f"W1 shape = {W1.shape}, b1 shape = {b1.shape}")
+print(f"W2 shape = {W2.shape}, b2 shape = {b2.shape}")
+print(f"W3 shape = {W3.shape}, b3 shape = {b3.shape}")
+
+#compile the model
+#   acc = track accuracy vs train/val sets
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    metrics=['acc'],
+)
+
+#run the fit, saving params to fitlog 
+#   epochs = N. cycles
+#   validation data is tested each epoch
+fitlog = model.fit(
+    timg,tlabels,
+    epochs=50,
+    validation_data = (vimg, vlabels), verbose=1
+)
+
+#extract metrics for plotting
+tacc = fitlog.history['acc']
+tloss = fitlog.history['loss']
+vacc = fitlog.history['val_acc']
+vloss = fitlog.history['val_loss']
+
+#evaluate model against reserved test data
+eval = model.evaluate(testimg, testlabels)
+
+print(
+  "EVAL\n"
+  f'test loss: {eval[0]}\n'
+  f'test acc:  {eval[1]}\n')
+
+
+#plot fit progress against train and validation data
+if POSTPLOT:
+  fig, ax = plt.subplots(1,2, figsize=(12,6))
+  fig.tight_layout(pad=2)
+
+  epochs = range(1, len(tacc) + 1)
+  ax[0].plot(epochs, tacc, 'r', label='Training')
+  ax[0].plot(epochs, vacc, 'b', label='Validation')
+  ax[0].set_title('Accuracy')
+  ax[0].legend()
+
+  ax[1].plot(epochs, tloss, 'r', label='Training')
+  ax[1].plot(epochs, vloss, 'b', label='Validation')
+  ax[1].set_title('Loss')
+  ax[1].legend()
+
+  plt.show()
 
 print("CLEAN EXIT")
 exit()
