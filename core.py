874ee7b6e33350e52a7b6e05dc36166f51e6cb23
lachlanc61 <lachlanc.612@gmail.com>
2022-11-26 23:13:02 +1000

    updated docs

--- core.py
+++ core.py
@@ -2,18 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import tensorflow as tf
-import tensorflow_datasets as tfds
-
-from tensorflow import keras
-from keras import Sequential 
-
 import src.utils as utils
 import src.tfmodel as tfmodel
 import src.vis as vis
 
 """
-Practice project - histology
+CNN classifier for histology
 
 """
 #-----------------------------------
@@ -38,24 +32,25 @@ spath, wdir, odir, checkpoint_path, checkpoint_dir = utils.initialise(config)
 #MAIN START
 #-----------------------------------
 
+#initialise the data into train, validation & test sets, and extract dataset info
+#   normalisation and augmentation applied here
 dtrain, dval, dtest, dsinfo = utils.datainit(config, DSETNAME)
 
+#extract a single image and print the batch size
 timg, tlabels, vimg, vlabels, testimg, testlabels = utils.batchcheck(dtrain, dval, dtest, batchsize)
 
-
+#produce an initial plot of 12 random images, if requested
 if config['PREPLOT']:
     vis.preplot(config, timg, tlabels)
 
-#we are overfitting pretty heavily, try regularisation
-l2reg=tf.keras.regularizers.L2(config['lamda'])
-#kernel_regularizer=l2reg
-  #not great, slow and not a big improvement, leave it out for now
-
-
+#build the model
+#   flags in config can be used to set hyperparameters
 model = tfmodel.build(config)
 
+#train the model, producing a log of the training process
 model, fitlog = tfmodel.train(model, dtrain, dval, config, checkpoint_path, odir)
 
+#produce a plot from a single random image, visualising a subset of the filters
 vis.layerplot(config, model, timg, tlabels, odir)
 
 print("CLEAN EXIT")
